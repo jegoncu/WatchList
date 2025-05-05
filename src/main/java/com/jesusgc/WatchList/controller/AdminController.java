6c0fa9288a4c69dde9bdba1bd84493654f8c1171
jegoncu <jegoncu@gmail.com>
2025-05-05 18:35:13 +0200

    vista basica admin

---
+++ src/main/java/com/jesusgc/WatchList/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.jesusgc.WatchList.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String mostrarAdminPanel(Model model) {  
+        return "admin"; 
+    }
+}
