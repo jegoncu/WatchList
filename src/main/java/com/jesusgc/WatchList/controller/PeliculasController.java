15b195fbd35ec5cd529a44869207f51eb5843cec
jegoncu <jegoncu@gmail.com>
2025-05-13 20:53:58 +0200

    Vista de peliculas

--- src/main/java/com/jesusgc/WatchList/controller/PeliculasController.java
+++ src/main/java/com/jesusgc/WatchList/controller/PeliculasController.java
@@ -1,15 +1,27 @@
 package com.jesusgc.WatchList.controller;
 
+import com.jesusgc.WatchList.model.Pelicula;
+import com.jesusgc.WatchList.service.PeliculaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
+import java.util.List;
+
 @Controller
 public class PeliculasController {
+
+    private final PeliculaService peliculaService;
+
+    public PeliculasController(PeliculaService peliculaService) {
+        this.peliculaService = peliculaService;
+    }
 
     @GetMapping("/peliculas")
     public String mostrarPeliculas(Model model) {
         model.addAttribute("currentPage", "peliculas");
+        List<Pelicula> peliculas = peliculaService.findAll();
+        model.addAttribute("peliculas", peliculas);
         return "peliculas";
     }
 }
