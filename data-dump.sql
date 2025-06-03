291d2c8a2a3b8aa042c652405e1b8499e32abcba
jegoncu <jegoncu@gmail.com>
2025-06-03 17:58:18 +0200

    error en plantilla de detalles

--- data-dump.sql
+++
@@ -1,4 +0,0 @@
---usuarios
-INSERT INTO watchlist.usuario (contrasenia,email,es_admin,es_publico,nombre) VALUES
-	 ('$2a$10$DPB.Nk2VNFouzHRTe1BD3OVu8.M.JW3dPcqtBUxvNTunJkzPpvWtW','admin@watchlist.com',1,1,'WatchList');
-
